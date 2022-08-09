b6415eac0d88a39245d89f2b9629692af533a4bd
peternontembeko <peternontembeko22@gmail.com>
2022-08-09 17:51:41 +0200

    Add project files.

---
+++ Indriver App/Indriver App/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Indriver_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        //  await Task.Delay(6000);
+
+        //            await Navigation.PushAsync(new LoginPage());
+    }
+
+    private async void LoginButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new LoginPage());
+    }
+}
+
+
+}
+
